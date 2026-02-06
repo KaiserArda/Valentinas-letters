f1465a75f0ca6a3d0096fb79d5750ab8886479c3
KaiserArda <ardadoruq@gmail.com>
2026-02-06 09:54:06 +0300

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -2,8 +2,8 @@ const CONFIGDATA = {
   titleHeader: "Hi, I have something to tell you. </br>  Thank you for making today special for me.",
   descriptionHeader:
     "I want to create more memories with you. Do you agree? :)",
-  buttonYes: "Do you accept?",
-  buttonNo: "Do you reject?",
+  buttonYes: "Yes",
+  buttonNo: "No",
   titleModar: "Thank you",
   descriptionModar:
     "Thank you for giving me the chance to be by your side, to protect you and to love you. </br> I know I'm not very handsome, maybe I'm not very good with girls, but I have a sincere heart. You are my WORLD </br> Even though I can't be with you tonight, my heart is with you. Just know that I'm thinking of you. Dress nicely for yourself, and when you look in the mirror, remember how much I love you. I have a little surprise for you, when the time is right.</br> Love you <3",

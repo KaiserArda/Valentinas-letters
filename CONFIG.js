6c7057c6bb4b4fba417d16ad6d0242afc06fdce4
KaiserArda <ardadoruq@gmail.com>
2026-02-14 00:31:29 +0300

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "No",
   titleModar: "Thank you",
   descriptionModar:
-    "Thank you for giving me the chance to be by your side, to protect you and to love you. </br> I know I'm not very handsome, maybe I'm not very good with girls, but I have a sincere heart. You are my WORLD </br> Even though I can't be with you tonight, my heart is with you. Just know that I'm thinking of you. Dress nicely for yourself, and when you look in the mirror, remember how much I love you. I have a little surprise for you, when the time is right.</br> Love you <3",
+    "Thank you for giving me the chance to be by your side, to protect you and to love you. </br> I know I'm not very handsome, maybe I'm not very good with girls, but I have a sincere heart. You are my WORLD </br> Even though I can't be with you tonight, my heart is with you. Just know that I'm thinking of you. Dress nicely for yourself, and when you look in the mirror, remember how much I love you. </br> Love you <3",
 };
